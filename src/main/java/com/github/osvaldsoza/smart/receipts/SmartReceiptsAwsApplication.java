d158cff2d2a34db092bb01d10d5490a82f0922ae
Osvaldo Ferreira <osvaldsoza@gmail.com>
2026-03-03 15:40:23 -0300

    First commit

--- src/main/java/com/github/osvaldsoza/smart/receipts/SmartReceiptsAwsApplication.java
+++ src/main/java/com/github/osvaldsoza/smart/receipts/SmartReceiptsAwsApplication.java
@@ -1,4 +1,4 @@
-package com.github.osvaldsoza.smart.receipts.aws;
+package com.github.osvaldsoza.smart.receipts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
